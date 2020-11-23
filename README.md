b6490759ff736e6a6695d0cdc59f5f3803b32c52
Michael Hutchings <67608821+MHutchbot@users.noreply.github.com>
2020-11-22 23:05:56 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+
+  
+  ![Badge for License](https://img.shields.io/badge/License-MIT%20License-green)
+
+  # readme
+  ## Description
+  Read me generator for DU bootcamp
+
+  ## Table of Contents
+  - [Usage](#Usage)
+  - [Test](#Test)
+  - [Installation](#Installation)
+  - [Contributing](#Contributing)
+  - [License](#License)
+  - [Questions](#Questions)
+
+  ## Usage
+  n/a
+
+  ## Test
+  npm test
+
+  ## Installation
+  npm install
+
+  ## Contributing
+  No contributors at this time. 
+
+  ## License
+  MIT License
+
+  ## Questions
+  Contact me by email at mhutchbot@gmail.com, or on GitHub at [@mhutchbot](https://www.github.com/mhutchbot)
---
+++ index.js
@@ -0,0 +1,86 @@
+// Dependencies
+const fs = require("fs");
+const inquirer = require("inquirer");
+const fileName = "README.md"
+
+// Modules
+const api = ("./api")
+const generateMarkdown = require("./utils/generateMarkdown");
+inquirer.registerPrompt('suggest', require('inquirer-prompt-suggest'));
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:"
+
+    },
+    {
+        type: "suggest",
+        name: "installation",
+        message: "What command should I run to install dependencies?",
+        suggestions: ['npm i']
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Any usage examples?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license will you use?",
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Are there contributing notes?"
+    },
+    {
+        type: "suggest",
+        name: "test",
+        message: "What command should be run to run tests?",
+        suggestions: ["npm test"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, response) {
+    // Writes the README file
+    fs.writeFile(fileName, response, function (err) {
+        // if error
+        if (err) {
+            return console.log(err);
+            // if success
+        } console.log("Success!")
+    })
+
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        writeToFile(fileName, generateMarkdown(response));
+    })
+}
+
+// function call to initialize program
+init();
